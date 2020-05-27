f2159b63dc4c840d171954bc71edb1149d330771
Daniel Zenkovitch <daniel.zenkovitch@hotmail.com>
2020-05-27 12:57:12 -0700

    Fix an issue with multiple makeStyles by adding chunks to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,75 @@
-'use strict';
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+"use strict";
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 6,
+      maxInitialRequests: 4,
+      automaticNameDelimiter: '~',
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
   devServer: {
     publicPath: "/",
     hot: true,
     proxy: {
-      '/api': {
+      "/api": {
         target: "https://us-central1-fashionable-typescript.cloudfunctions.net",
         default: false,
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
-  mode: 'development',
-  entry: './src/index.tsx',
+  mode: "development",
+  entry: { main: "./src/index.tsx", vendor: ["@material-ui/styles"] },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: __dirname + '/src/index.html',
-      inject: 'body'
+      title: "Output Management",
+      template: __dirname + "/src/index.html",
+      inject: "body",
     }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "vendor",
+    //   filename: "mstyles.js",
+    //   minChunks: Infinity,
+    // }),
   ],
-	output: {
-		filename: '[id].main.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader'
-            },
-            {
-              test: /\.css$/i,
-              use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '.ts', '.tsx', '.js' ]
-    }
+  output: {
+    filename: "[id].main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
 };
